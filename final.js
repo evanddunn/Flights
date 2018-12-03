c3657428227c933603b8da1e5f23acd83e5c8068
Karley Wheeler <karwheel@live.unc.edu>
2018-12-03 00:10:35 -0500

    update final.js
    added functionality of getting values from input of whereTo, whereFrom, and date

--- final.js
+++ final.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-    alert("Hey");
+    let body = $('body');
+    let root = "http://comp426.cs.unc.edu:3001";
+    $.ajax(root + '/sessions', {
+        type: 'POST',
+        data: {'user': {
+            'username': 'karwheel',
+            'password': 'comp426',
+        }},
+        dataType: 'json',
+        xhrFields: {withCredientials: true},
+    });
+
+    $('button.w3-button.w3-dark-grey').click(function() {
+        alert('howdy');
+        let depart = $('#departingInputInput').val();
+        let arrive = $('#arrivingInputInput').val();
+        let date = $('#dateInputInput').val();
+        alert("depart: "+depart+" arrive: "+arrive+" date: "+date);
+    });
 })
