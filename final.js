2768c747c66aee30c9f906b07fb77f795bdb5ea9
edd <evanddunn@gmail.com>
2018-12-06 16:32:19 -0500

    fixed ajax calls

--- final.js
+++ final.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
     let body = $('body');
     let root = "http://comp426.cs.unc.edu:3001";
-    $.ajax(root + '/sessions', {
+    $.ajax({
+        url: root + '/sessions',
         type: 'POST',
-        data: {'user': {
-            'username': 'karwheel',
-            'password': 'comp426',
-        }},
-        dataType: 'json',
-        xhrFields: {withCredientials: true},
-    });
+        data: {
+          "user": {
+            "username": "karwheel",
+            "password": "comp426"
+          }
+        },
+        xhrFields: { withCredentials: true }
+      });
 
     $('button.w3-button.w3-dark-grey').click(function() {
         alert('howdy');
@@ -17,5 +19,12 @@ $(document).ready(function () {
         let arrive = $('#arrivingInputInput').val();
         let date = $('#dateInputInput').val();
         alert("depart: "+depart+" arrive: "+arrive+" date: "+date);
+        $.ajax({
+            url: root +'/flights',
+            type: 'GET',
+            xhrFields: { withCredentials: true }
+          }).done(function(data) {
+              console.log(data[0]);
+          });
     });
 })
