a82468021300e8b0de2df9d40912f2259a81c29c
edd <evanddunn@gmail.com>
2018-12-06 18:47:15 -0500

    get all instances of flights to and from specified locations

--- final.js
+++ final.js
@@ -5,26 +5,60 @@ $(document).ready(function () {
         url: root + '/sessions',
         type: 'POST',
         data: {
-          "user": {
+        "user": {
             "username": "karwheel",
             "password": "comp426"
-          }
+        }
         },
         xhrFields: { withCredentials: true }
-      });
+    });
 
     $('button.w3-button.w3-dark-grey').click(function() {
         alert('howdy');
-        let depart = $('#departingInputInput').val();
-        let arrive = $('#arrivingInputInput').val();
+        let depart = $('#departingInputInput').val().toUpperCase();
+        let arrive = $('#arrivingInputInput').val().toUpperCase();
         let date = $('#dateInputInput').val();
+        let depID, arrID;
+        let flights = new Array();
+        let instances = new Array();
         alert("depart: "+depart+" arrive: "+arrive+" date: "+date);
         $.ajax({
-            url: root +'/flights',
+            url: root +'/airports?filter[code]=' + depart,
             type: 'GET',
             xhrFields: { withCredentials: true }
-          }).done(function(data) {
-              console.log(data[0]);
-          });
+        }).done(function(data) {
+            //console.log(data[0]);
+            depID = data[0].id;
+            $.ajax({
+                url: root +'/airports?filter[code]=' + arrive,
+                type: 'GET',
+                xhrFields: { withCredentials: true }
+            }).done(function(data) {
+                //console.log(data[0]);
+                arrID = data[0].id;
+                $.ajax({
+                    url: root + '/flights?filter[departure_id]=' +depID,
+                    type: 'GET',
+                    xhrFields: { withCredentials: true }
+                }).done(function (data) {
+                    for(let i = 0; i < data.length; i++) {
+                        if(data[i].arrival_id == arrID) flights.push(data[i]);
+                    }
+                    for(let i = 0; i < flights.length; i++) {
+                        $.ajax({
+                            url: root + '/instances?filter[flight_id]=' + flights[i].id,
+                            type: 'GET',
+                            xhrFields: { withCredentials: true }
+                        }).done(function (data) {
+                            for(let j = 0; j < data.length; j++) instances.push(data[j])
+                        }).done(function () {
+                            for(let i = 0; i < instances.length; i++) {
+                                console.log(instances[i].id);
+                            }
+                        });
+                    }    
+                });
+            });
+        });
     });
 })
