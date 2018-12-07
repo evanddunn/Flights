154e8f2773910d537d79efb5f26ce47a768d34f4
edd <evanddunn@gmail.com>
2018-12-06 22:40:40 -0500

    cards populate with database data

--- final.js
+++ final.js
@@ -43,20 +43,46 @@ $(document).ready(function () {
                 }).done(function (data) {
                     for(let i = 0; i < data.length; i++) {
                         if(data[i].arrival_id == arrID) flights.push(data[i]);
+                        //flights = data;
                     }
-                    for(let i = 0; i < flights.length; i++) {
+                    console.log(flights)
+                    for(let i = 0; i < flights.length; i++){
                         $.ajax({
                             url: root + '/instances?filter[flight_id]=' + flights[i].id,
                             type: 'GET',
-                            xhrFields: { withCredentials: true }
+                            xhrFields: { withCredentials: true },
+                            async: false
                         }).done(function (data) {
-                            for(let j = 0; j < data.length; j++) instances.push(data[j])
-                        }).done(function () {
-                            for(let i = 0; i < instances.length; i++) {
-                                console.log(instances[i].id);
+                            for(let j = 0; j < data.length; j++) {
+                                if(data[j].date == date) {
+                                    data[j].info = i;//`${flights[i].departs_at} ${flights[i].arrives_at} ${flights[i].airline_id}`;
+                                    instances.push(data[j]);
+                                }
                             }
                         });
-                    }    
+                    }
+                    console.log(instances[0].info);
+                    for(let i = 1; i <= instances.length; i++){
+                        //console.log(flights[i].departs_at);
+                        let airline;
+                        let flightcard = $('#card' + i);
+                        let dep_at = flights[instances[i-1].info].departs_at;//instances[i-1].info.substring(11, 16);
+                        let arr_at = flights[instances[i-1].info].arrives_at;
+                        let airline_id = flights[instances[i-1].info].airline_id;
+                        $.ajax({
+                            url: root + '/airlines/' + airline_id,
+                            type: 'GET',
+                            xhrFields: { withCredentials: true },
+                            async: false
+                        }).done(function (data) {
+                            airline = data.name;
+                        });
+                        flightcard.children('h3').text(`${depart} to ${arrive}`)
+                        flightcard.children('span.depart').text(`Departs: ${dep_at.substring(11, 16)}`);
+                        flightcard.children('span.arrive').text(`Arrives: ${arr_at.substring(11, 16)}`);
+                        flightcard.children('p.airline').text(`Airline: ${airline}`);
+                        flightcard.toggle();
+                    }
                 });
             });
         });
